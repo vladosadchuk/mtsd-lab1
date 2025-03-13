5a27680b2943dcea502b1a85456233c8884fb49c
vladosadchuk <osadchukvlad19@gmail.com>
2025-03-13 17:42:28 +0200

    Add reading coefficients from a file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
+import os
+import sys
 
 
 def solve_quadratic(a, b, c):
@@ -26,5 +28,19 @@ def interactive_mode():
         print(f"x{i} = {root}")
 
 
+def file_mode(filepath):
+    with open(filepath, 'r') as file:
+        content = file.read().strip()
+        a, b, c = map(float, content.split())
+        print(f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0")
+        roots = solve_quadratic(a, b, c)
+        print(f"There are {len(roots)} roots")
+        for i, root in enumerate(roots, 1):
+            print(f"x{i} = {root}")
+
+
 if __name__ == "__main__":
-    interactive_mode()
+    if len(sys.argv) == 1:
+        interactive_mode()
+    elif len(sys.argv) == 2:
+        file_mode(sys.argv[1])
